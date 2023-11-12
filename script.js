fd9ae9c939dacc3f77bdfa50f53ed0c07e38d480
Tyler <122329987+ty-hoffman@users.noreply.github.com>
2023-11-12 16:11:55 -0500

    Multiple Updates: Add form to HTML and buttons to open and close form Added JS functions to open and close dialog CSS form styling

--- script.js
+++ script.js
@@ -14,3 +14,33 @@ function Book (title, author, pages, read) {
 function addBookToLibrary (bookToAdd) {
     myLibrary.push(bookToAdd);
 }
+
+function populateBookGrid () {
+
+    /* clearing out grid */
+    emptyGridOfBooks();
+    
+    let list = document.querySelector('.booksList');
+    let newListElement = document.createElement('li')
+    newListElement.classList.add('bookItem');
+    myLibrary.forEach((book) => {
+        newListElement.textContent = book.info();
+    })
+}
+
+/* function to delete books from DOM */
+function emptyGridOfBooks () {
+    let allBooks = document.querySelectorAll('.bookItem');
+    allBooks.remove();
+}
+
+let showFormSelector = document.querySelector('#openFormButton');
+let closeFormSelector = document.querySelector('#closeFormButton');
+let dialog = document.querySelector('dialog');
+
+showFormSelector.addEventListener('click', () => {
+    dialog.showModal();
+})
+closeFormSelector.addEventListener('click', () => {
+    dialog.close();
+})
